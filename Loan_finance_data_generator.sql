2e5df4ee0758ff67bd960e4cdc3b399becacccfb
iplatun <76597496+iplatun@users.noreply.github.com>
2021-01-18 13:45:40 -0600

    Update Loan_finance_data_generator.sql

--- Loan_finance_data_generator.sql
+++ Loan_finance_data_generator.sql
@@ -1,8 +1,9 @@
-SET SERVEROUTPUT ON;
+DECLARE
+    tmp := 0;
 BEGIN
-   FOR i IN 1..30 LOOP
-   i := i + 1;
-      INSERT INTO CUSTOMER_INFO VALUES (DBMS_RANDOM.value (100000000, 999999999), DBMS_RANDOM.string('first_name', DBMS_RANDOM.value (1, 30)));
-      END LOOP;
-      END;
-      /
+  FOR i IN 1..30 LOOP
+  tmp := 100000000 + trunc(dbms_random.value(1, 899999999));
+     INSERT INTO CUSTOMER_INFO (CUSTOMER_SSN) VALUES (DBMS_OUTPUT.put_line(tmp));
+  END LOOP;
+END;
+/
