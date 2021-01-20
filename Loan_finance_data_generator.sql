a9fee562aa2378aeefae372a852c15f8decbceac
iplatun <76597496+iplatun@users.noreply.github.com>
2021-01-20 15:18:52 -0500

    Delete Loan_finance_data_generator.sql

--- Loan_finance_data_generator.sql
+++
@@ -1,8 +0,0 @@
-SET SERVEROUTPUT ON;
-BEGIN
-   FOR i IN 1..30 LOOP
-   i := i + 1;
-      INSERT INTO CUSTOMER_INFO VALUES (DBMS_RANDOM.value (100000000, 999999999), DBMS_RANDOM.string('first_name', DBMS_RANDOM.value (1, 30)));
-      END LOOP;
-      END;
-      /
